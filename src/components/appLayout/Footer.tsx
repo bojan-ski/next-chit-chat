37d9fd4ac4248b3b787785ed24b232abc5841e89
Bojan SKI <codeucenje@gmail.com>
2025-08-04 10:16:49 +0200

    chadcn package installed. layout updated. footer component created

---
+++ src/components/appLayout/Footer.tsx
@@ -0,0 +1,18 @@
+import { JSX } from "react";
+
+function Footer(): JSX.Element {
+    return (
+        <footer className="bg-[#FDF6F0] dark:bg-[#3B2F2F] border-t border-[#E5C6AC] dark:border-[#7C5C53] py-5 shadow-sm">
+            <div className="text-center text-sm space-y-1 max-w-xl mx-auto px-4">
+                <p className="font-semibold text-[#7B4B3A] dark:text-[#E8D3C4] text-lg tracking-wide">
+                    &copy; 2025 <span className="text-[#C05C41] dark:text-[#D9997A]">Chit Chat</span>
+                </p>
+                <p className="text-[#A3776E] dark:text-[#BBAA9F] font-medium">
+                    All rights reserved | <span className="text-[#A05236] dark:text-[#C28B6B]">code BPdevelopment</span>
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/appLayout/Header.tsx
@@ -0,0 +1,11 @@
+import { JSX } from "react";
+
+function Header(): JSX.Element {
+  return (
+    <header>
+      Header
+    </header>
+  )
+}
+
+export default Header
