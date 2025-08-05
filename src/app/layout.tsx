c05fda4c8bf2ac4ee331a6e3ca496cdb97cabb23
Bojan SKI <codeucenje@gmail.com>
2025-08-05 09:10:49 +0200

    cleck package installed, middleware file created (init setup)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Merriweather, Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/appLayout/Header";
 import Footer from "@/components/appLayout/Footer";
 
@@ -27,16 +28,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${merriweather.variable} ${inter.variable} antialiased`}>
-        <Header />
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${merriweather.variable} ${inter.variable} antialiased`}>
+          <Header />
 
-        <main>
-          {children}
-        </main>
+          <main>
+            {children}
+          </main>
 
-        <Footer />
-      </body>
-    </html>
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
